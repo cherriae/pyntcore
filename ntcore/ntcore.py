40be259e78363c999d8649440441c9b92f3073dc
Jerry <zeruiz@nycstudents.net>
2024-05-22 21:46:35 -0400

    Docstrings

--- ntcore/ntcore.py
+++ ntcore/ntcore.py
@@ -14,6 +14,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class NTCore:
+    """
+    Class representing the core functionality of NTCore.
+    """
     def __init__(self, team_id: int):
         self.topics: Dict[str, Topic] = {}
         self.socket: socket.socket = None
@@ -21,17 +24,57 @@ class NTCore:
         self._team_address: str = f"roborio-{team_id}-frc.local"
 
     async def get_instance_by_team(self, team_address: str = None, port: int = 5810):
+        """
+        Get an instance of NTCore by team address.
+
+        Args:
+        - team_address: str, optional
+        - port: int
+
+        Returns: Instance of NTCore
+        """
+
         return await self._connect(team_address or self._team_address, port)
 
     async def _connect(self, address: str, port: int):
+        """
+        Establish a socket connection to the specified address and port.
+
+        Args:
+        - address: str
+        - port: int
+
+        Returns: Instance of NTCore
+        """
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         await asyncio.get_event_loop().sock_connect(self.socket, (address, port))
         return self
 
     def create_topic(self, name: str, type_info: NetworkTablesType, default_value: Any = None) -> 'Topic':
+        """
+        Create a topic and add it to the topics dictionary.
+
+        Args:
+        - name: str
+        - type_info: NetworkTablesType
+        - default_value: Any
+
+        Returns: Instance of Topic
+        """
+
         topic = Topic(self, name, type_info, default_value)
         self.topics[name] = topic
         return topic
 
     async def send(self, message: Dict[str, Any]):
+        """
+        Send a message through the socket.
+
+        Args:
+        - message: Dict[str, Any]
+
+        Returns: None
+        """
+
         await asyncio.get_event_loop().sock_sendall(self.socket, json.dumps(message).encode('utf-8'))
--- ntcore/topic.py
+++ ntcore/topic.py
@@ -10,6 +10,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class Topic:
+    """
+    Represents a topic in NTCore with methods to subscribe, publish, set value, and publish value. 
+    Includes functionality to handle subscribers and publishing values asynchronously.
+    """
     def __init__(self, ntcore: NTCore, name: str, type_info: NetworkTablesType, default_value: Any):
         self.ntcore: NTCore = ntcore
         self.name: str = name
@@ -22,22 +26,65 @@ class Topic:
             self.publish_value(default_value)
 
     def subscribe(self, callback: Callable[[Any], None], immediate_notify: bool = False):
+        """
+        Subscribe a callback to the topic.
+
+        Adds a callback function to the list of subscribers for the topic. 
+        If immediate_notify is True and a default value is set, the callback is immediately triggered with the default value.
+
+        Args:
+        - callback: Callable[[Any], None]
+        - immediate_notify: bool, optional
+
+        Returns: None
+        """
         self.subscribers.append(callback)
         if immediate_notify and self.default_value is not None:
             callback(self.default_value)
 
     def publish(self, properties: Dict[str, Any] = None):
+        """
+        Set a value for the topic.
+
+        Args:
+        - value: Any
+
+        Raises:
+        - ValueError: If trying to set value on an unpublished topic
+
+        Returns: None
+        """
         self.publisher = True
         if self.default_value is not None:
             self.publish_value(self.default_value)
 
     def set_value(self, value: Any):
+        """
+        Publish a value for the topic.
+
+        Args:
+        - value: Any
+
+        Raises:
+        - ValueError: If trying to publish value on an unpublished topic
+
+        Returns: None
+        """
+
         if self.publisher:
             self.publish_value(value)
         else:
             raise ValueError("This topic is not published")
 
     async def publish_value(self, value: Any):
+        """
+        Publish a value to the subscribers of the topic.
+
+        Args:
+        - value: Any
+
+        Returns: None
+        """
         message = {
             'name': self.name,
             'type': self.type_info.value,
